d3d0d9d0e84e13afc56e5ade5b8b1d7f92ac44b9
Pavel Ershov <pershov@game-inisght.com>
2020-07-13 19:27:36 +0300

    OSX privacy clean example

---
+++ osx_clean_privacy/clean.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# https://apple.stackexchange.com/questions/339363/how-can-i-remove-applications-from-security-privacy
+
+#APP_ID=$(osascript -e 'id of app "Bitbar"')
+
+# APP_ID=$(osascript -e 'id of app "$1"')
+# echo "App id is: $APP_ID"
+# tccutil reset All $APP_ID
+
+tccutil reset All
