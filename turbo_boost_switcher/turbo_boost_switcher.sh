630cd4378ed55a902741e09f50ab8d788af38446
DevNul <devnulpavel@gmail.com>
2020-11-17 22:01:23 +0300

    Some improvement in script

--- turbo_boost_switcher/turbo_boost_switcher.sh
+++ turbo_boost_switcher/turbo_boost_switcher.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-sudo /Applications/Turbo\ Boost\ Switcher.app/Contents/MacOS/Turbo\ Boost\ Switcher 2&>1 /dev/null &
+sudo /Applications/Turbo\ Boost\ Switcher.app/Contents/MacOS/Turbo\ Boost\ Switcher >/dev/null 2>&1 &
